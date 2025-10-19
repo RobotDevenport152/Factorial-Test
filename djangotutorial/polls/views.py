06f26042d1bb42ba9593ff1a8284965bae9cd8bc
RobotDevenport152 <jjun712@aucklanduni.ac.nz>
2025-10-19 19:45:31 +1300

    Djangotutorial

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -4,4 +4,30 @@ def welcome(request, name):
     A simple Django view that takes a name from the URL
     and returns a personalized welcome message.
     """
-    return HttpResponse(f"<h1>Welcome {name} to Django!</h1>")
+    html = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Hello Django</title>
+            <style>
+                body {
+                    background-color: green;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                }
+                h1 {
+                    color: red;
+                    font-family: Arial, sans-serif;
+                    font-size: 3em;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Hello, World!</h1>
+        </body>
+        </html>
+        """
+    return HttpResponse(html)
